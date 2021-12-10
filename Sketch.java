475af1123b26e6a5893b2bf066775c666b030ce9
Alex Young <Alex Young@10.8.164.231>
2021-12-10 14:18:24 -0500

    Submit

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(500, 500);
+    size(400, 400);
   }
 
   /** 
@@ -16,7 +16,14 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255,250,250);
+    
+    // White background
+    noStroke();
+    fill(40, 50, 200);
+    rect(0, 0, 400, 200);
+    
+    stroke(0);
   }
 
   /**
@@ -24,13 +31,56 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	  // Bottom snow ball
+	  fill(255,250,250);
+	  ellipse(200, 200, 150, 150);
+	  
+	  // Middle snow ball
+	  fill(255,250,250);
+	  ellipse(200, 100, 100, 100);
+	  
+	  // Top snow ball
+	  fill(255,250,250);
+	  ellipse(200, 50, 75, 75);
+	  
+	  // Left eye
+	  fill(0,0,0);
+	  ellipse(190, 40, 5, 5);
+	  
+	  // Right eye
+	  fill(0,0,0);
+	  ellipse(215, 40, 5, 5);
+	  
+	  // Bottom button
+	  fill(0,0,0);
+	  ellipse(200, 215, 15, 15);
+	  
+	  // Middle button 
+	  fill(0,0,0);
+	  ellipse(200, 180, 15, 15);
+	  
+	  // Top button
+	  fill(0,0,0);
+	  ellipse(200, 110, 15, 15);
+	  
+	  // Smiley face
+	  fill(0,0,0);
+	  arc(200, 50, 10, 10, 0, PI); 
+	  
+	  // Sun
+	  fill(255,255,0);
+	  ellipse(390, 10, 50, 50);
+	  
+	  // Left arm
+	  line(50, 50, 150, 100);
+	  
+	  // Right arm
+	  line(250, 100, 350, 50);
+	  
+	  
+	 
   }
   
-  // define other methods down here.
+  
+  
 }
